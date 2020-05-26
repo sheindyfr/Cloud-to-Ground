3e562a2e1fabf0f0e5f567affb5d355d2bea4cc3
sheindyfr <sheindy33@gmail.com>
2020-05-26 13:39:23 +0300

    Connect to socket

--- Controller.cpp
+++ Controller.cpp
@@ -3,6 +3,11 @@
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
-	Server::Server();
+	std::cout << "----------------------------------------" << std::endl;
+	std::cout << "------------Server is running-----------" << std::endl;
+	std::cout << "----------------------------------------" << std::endl;
+	
+	char * data = NULL;
+	Server::Server(&data);
+	std::cout << "the data:\n" << data << std::endl;
 }
